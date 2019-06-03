c8bfe65065703b07a0ed89340be79acbb50be7cf
nhattq <demonslight998@gmail.com>
2019-06-03 22:04:03 +0700

    :rocket: Update tslint.json

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-const inquirer = require("inquirer");
-const { writeFileSync } = require("fs");
+const inquirer = require('inquirer')
+const { writeFileSync } = require('fs')
 
 const tsconfigs = {
-  react: JSON.stringify(require("./config/tsconfig.react.json"), null, 2),
-  "react-native": JSON.stringify(
-    require("./config/tsconfig.react-native.json"),
+  react: JSON.stringify(require('./config/tsconfig.react.json'), null, 2),
+  'react-native': JSON.stringify(
+    require('./config/tsconfig.react-native.json'),
     null,
-    2
+    2,
   ),
-  node: JSON.stringify(require("./config/tsconfig.react-native.json"), null, 2)
-};
+  node: JSON.stringify(require('./config/tsconfig.react-native.json'), null, 2),
+}
+const tsLint = JSON.stringify(require('./config/tslint.json', null, 2))
 
 inquirer
   .prompt([
     {
-      type: "list",
+      type: 'list',
       message: "Pick the framework you're using:",
-      name: "framework",
-      choices: ["react", "react-native", "node"]
-    }
+      name: 'framework',
+      choices: ['react', 'react-native', 'node'],
+    },
   ])
   .then(({ framework }) => {
-    const tsconfigToWrite = tsconfigs[framework];
+    const tsconfigToWrite = tsconfigs[framework]
+    const tsLintToWrite = tsLint
+    const cwd = process.cwd()
+    writeFileSync(cwd + '/tsconfig.json', tsconfigToWrite)
+    writeFileSync(cwd + '/tslint.json', tsLintToWrite)
 
-    const cwd = process.cwd();
-    writeFileSync(cwd + "/tsconfig.json", tsconfigToWrite);
-    console.log("tsconfig.json successfully created");
-  });
+    console.log('tsconfig.json and tslint.json successfully created')
+  })
